319458a7372f4cdd0e28823541f5cc723856d943
Martin Rada <martinradadl@gmail.com>
2024-01-16 16:00:08 -0500

    Lesson preview updated

--- src/pages/lesson/lesson-preview.js
+++ src/pages/lesson/lesson-preview.js
@@ -6,33 +6,37 @@ import { LessonPreviewDropdown } from "./lesson-preview-dropdown";
 import { DeleteLessonDialog } from "./delete-lesson-dialog";
 
 export const LessonPreview = (props) => {
-  const { isCurrentLesson, lesson } = props;
+  const { isCurrentLesson, lesson, index, user } = props;
   const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
 
   return (
-    <div className="lesson-preview-container">
+    <div className="lesson-preview-container button">
       <div className="lesson-preview">
         <AiFillYoutube id="yt-icon" />
-        <p>{lesson.title}</p>
+        <p>{`${index + 1}. ${lesson.title}`}</p>
         <div className="actions-container">
           {isCurrentLesson ? (
             <button id="resume-lesson-button">Continuar</button>
           ) : null}
-          <LessonPreviewDropdown
-            openEditDialog={setIsEditDialogOpen}
-            openDeleteDialog={setIsDeleteDialogOpen}
-          />
-          <EditLessonDialog
-            selectedLesson={lesson}
-            isOpen={isEditDialogOpen}
-            setIsOpen={setIsEditDialogOpen}
-          />
-          <DeleteLessonDialog
-            isOpen={isDeleteDialogOpen}
-            setIsOpen={setIsDeleteDialogOpen}
-            id={lesson._id}
-          />
+          {user?.type === "instructor" ? (
+            <div>
+              <LessonPreviewDropdown
+                openEditDialog={setIsEditDialogOpen}
+                openDeleteDialog={setIsDeleteDialogOpen}
+              />
+              <EditLessonDialog
+                selectedLesson={lesson}
+                isOpen={isEditDialogOpen}
+                setIsOpen={setIsEditDialogOpen}
+              />
+              <DeleteLessonDialog
+                isOpen={isDeleteDialogOpen}
+                setIsOpen={setIsDeleteDialogOpen}
+                id={lesson._id}
+              />
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
