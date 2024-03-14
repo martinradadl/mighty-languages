8e2dd89a88cb2a24de08d137427fbf1bd6546930
Martin Rada <martinradadl@gmail.com>
2024-03-14 17:08:29 -0500

    old files deleted

--- src/components/questionnaire/form.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Question } from "./question";
-
-export const Form = () => {
-    return <div>
-        <Question/>
-    </div>
-}
--- src/components/questionnaire/question-checker.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { AiFillCheckCircle, AiFillCloseCircle } from "react-icons/ai";
-
-export const QuestionChecker = (props) => {
-  const { isCorrect } = props;
-  return isCorrect ? <AiFillCheckCircle /> : <AiFillCloseCircle />;
-};
