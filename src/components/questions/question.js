19b2faea8ded8a4a78ae82521980b526a61a6e96
Martin Rada <martinradadl@gmail.com>
2024-03-31 15:21:26 -0500

    Question component updated

--- src/components/questions/question.js
+++ src/components/questions/question.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { MultipleChoiceQuestion } from "./multiple-choice-question";
+import { FillingQuestion } from "./filling-question";
 
-export const Question = () => {
-  return <p>Question</p>;
+export const Question = (props) => {
+  const { selectedQuestion } = props;
+  const dispatch = useDispatch();
+
+  if (selectedQuestion.type.id === "MULT_CHOICE") {
+    return <MultipleChoiceQuestion selectedQuestion={selectedQuestion} />;
+  } else {
+    return <FillingQuestion selectedQuestion={selectedQuestion} />;
+  }
 };
