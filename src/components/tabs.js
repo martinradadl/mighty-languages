b6d7f23d5afab774cac1f35a69bb9e85d6592ee0
Martin Rada <martinradadl@gmail.com>
2024-04-15 12:28:15 -0500

    ClassNames added to tabs component

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,13 +1,18 @@
 import { Tab } from "@headlessui/react";
 import React from "react";
+import "../styles/global.css";
 
 export function Tabs(props) {
   const { tabs } = props;
   return (
     <Tab.Group>
-      <Tab.List>
+      <Tab.List className="tabs-titles-container">
         {tabs.map((tab, i) => {
-          return <Tab key={i}>{tab.title}</Tab>;
+          return (
+            <Tab key={i} className="tab-title">
+              {tab.title}
+            </Tab>
+          );
         })}
       </Tab.List>
       <Tab.Panels>
