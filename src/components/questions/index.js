c661a7fa9e7a2755e0ecebb4099f87701c30f609
Martin Rada <martinradadl@gmail.com>
2024-05-07 20:18:00 -0500

    Quiz Tab elements restrictions added, as well as empty quiz message

--- src/components/questions/index.js
+++ src/components/questions/index.js
@@ -41,40 +41,45 @@ export const QuizTab = (props) => {
   }, [handleGetQuestions]);
 
   return (
-    <div>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          margin: "10px 0px",
-        }}
+    <div style={{ marginTop: "20px" }}>
+      <LoadingWrapper
+        isLoading={questionsList === null || status === "loading"}
       >
-        <AddQuestionDialog />
         {user?.type === "instructor" || user?.type === "admin" ? (
           <div
             style={{
               display: "flex",
-              gap: "12px",
               alignItems: "center",
+              justifyContent: "space-between",
+              marginBottom: "10px",
             }}
           >
-            <p>Activar Quiz:</p>
-            <Switch
-              checked={isQuizActive}
-              onChange={handleChangeIsQuizActive}
-              className={isQuizActive ? "switch active" : "switch"}
-            >
+            <AddQuestionDialog />
+            {questionsList.length > 0 ? (
               <div
-                className={isQuizActive ? "switch-ball active" : "switch-ball"}
-              />
-            </Switch>
+                style={{
+                  display: "flex",
+                  gap: "12px",
+                  alignItems: "center",
+                }}
+              >
+                <p style={{ margin: "0px" }}>Activar Quiz:</p>
+                <Switch
+                  checked={isQuizActive}
+                  onChange={handleChangeIsQuizActive}
+                  className={isQuizActive ? "switch active" : "switch"}
+                >
+                  <div
+                    className={
+                      isQuizActive ? "switch-ball active" : "switch-ball"
+                    }
+                  />
+                </Switch>
+              </div>
+            ) : null}
           </div>
         ) : null}
-      </div>
-      <LoadingWrapper
-        isLoading={questionsList === null || status === "loading"}
-      >
+
         {questionsList.map((question, i) => {
           return (
             <Question
@@ -83,16 +88,30 @@ export const QuizTab = (props) => {
                 question,
                 index: i + 1,
                 isInstructor: isInstructor,
+                isAdmin: user?.type === "admin",
                 userAnswers,
                 setUserAnswers,
               }}
             />
           );
         })}
-        {questionsList.length > 0 && !isInstructor ? (
+        {questionsList.length === 0 ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              margin: "10px 0px",
+            }}
+          >
+            <h4 style={{ margin: "0px" }}>
+              Por el momento no hay preguntas disponibles
+            </h4>
+          </div>
+        ) : !isInstructor ? (
           <button
             type="button"
             className="open-dialog-button"
+            style={{ marginTop: "10px" }}
             onClick={(event) => {
               handleCompleteLesson();
               console.log(userAnswers);
