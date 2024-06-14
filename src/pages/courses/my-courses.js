c163fbf7e1b09c6640c9ba9458af9db58a4036fb
Martin Rada <martinradadl@gmail.com>
2024-06-14 18:59:49 -0500

    Unnecessary TabsContent component removed, elements inside were passed to my-courses component

--- src/pages/courses/my-courses.js
+++ src/pages/courses/my-courses.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "../../styles/courses/course.css";
-import { TabsContent } from "./tabs-content";
+import { Tabs } from "../../components/tabs";
+import { CommentsTab } from "../../components/comments";
+import { QuizTab } from "../../components/questions";
+import { OnProgressTab } from "./on-progress-tab";
+import { CompletedTab } from "./completed-tab";
 
 export const MyCourses = () => {
+  const tabs = [
+    {
+      title: "En progreso",
+      content: <OnProgressTab />,
+    },
+    { title: "Completados", content: <CompletedTab /> },
+  ];
 
   return (
     <div className="my-courses-container">
       <h1 style={{ fontSize: "2rem" }}>Mis Cursos</h1>
-      <TabsContent />
+      <Tabs tabs={tabs} isTags />
     </div>
   );
 };
